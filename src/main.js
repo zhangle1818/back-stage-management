15f33ce3a2b83fd2ed9ff73ecc97e16ae511421f
zhangle <1427431747@qq.com>
2019-04-30 16:19:05 +0800

    02.完成登陆页面和成功登陆后跳转首页

--- src/main.js
+++ src/main.js
@@ -3,9 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import axios from 'axios'
 
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+
+// 设置基准路径
+axios.defaults.baseURL = 'http://localhost:8899'
+// 是否是生成环境
+axios.defaults.withCredentials = true
+
+// 将axios绑定到原型上，每一个组件都可以通过this.$axios访问到
+Vue.prototype.$axios = axios
+
+// 添加拦截器
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next()
+  }
+  if (!localStorage.getItem('userInfo')) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 
 Vue.use(iView)
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,127 @@
 <template>
-    <div>Login</div>
+  <div class="login">
+    <div class="big"></div>
+    <Card style="width:350px">
+      <p slot="title">
+        <Icon type="logo-designernews"></Icon>
+        后台管理系统
+      </p>
+      <Form
+        ref="formInline"
+        :model="formInline"
+        :rules="ruleInline"
+      >
+        <FormItem prop="user">
+          <Input
+            type="text"
+            v-model="formInline.user"
+            placeholder="Username"
+          >
+          <Icon
+            type="ios-person-outline"
+            slot="prefix"
+          ></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input
+            type="password"
+            v-model="formInline.password"
+            placeholder="Password"
+          >
+          <Icon
+            type="ios-lock-outline"
+            slot="prefix"
+          ></Icon>
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            @click="handleSubmit('formInline')"
+          >登陆</Button>
+          <Button
+            @click="handleReset('formInline')"
+            style="margin-left: 8px"
+          >重置</Button>
+        </FormItem>
+        </FormItem>
+      </Form>
+    </Card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formInline: {
+        user: 'admin',
+        password: '123456'
+      },
+      ruleInline: {
+        user: [
+          { required: true, message: '请填写用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请填写密码.', trigger: 'blur' },
+          { type: 'string', min: 6, message: '密码的长度不能小于6', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+        // 发起请求获取获取登陆的数据
+          this.$axios({
+            url: '/admin/account/login',
+            method: 'POST',
+            data: {
+              uname: this.formInline.user,
+              upwd: this.formInline.password
+            }
+          }).then(result => {
+            console.log(result)
+            /** 判断如果登陆成功:1.将用户的信息保存到本地2.跳转到admin页面 */
+            if (result.data.status === 0) {
+              localStorage.setItem('userInfo', JSON.stringify(result.data.message))
+              this.$router.push('/admin')
+              this.$Message.success('登陆成功')
+            } else {
+              this.$Message.success('登陆失败')
+            }
+          })
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scopcd lang="less">
+.login {
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  > .big {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: url(../assets/1.jpg) no-repeat center;
+    background-size: cover;
+    // transform: scale(1.02)
+  }
+  > .ivu-card {
+    position: absolute;
+    top: 66px;
+    right: 450px;
+  }
+}
 </style>
